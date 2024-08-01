46ebcd0c4ea72ab60d7d738f6cb1f11f05d98cdd
20105070 <87380731+20105070@users.noreply.github.com>
2024-08-01 09:23:06 +0100

    Commit Made for Leavers 16.0

--- Made_for_Leavers/src/app/http_service.ts
+++ Made_for_Leavers/src/app/http_service.ts
@@ -18,23 +18,20 @@ export class HttpService {
   deleteUniversity(email: string, name: string): Observable<any> {
     let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*");
     headers = headers.set("Authorization", email);
-    /*return this.httpClient.delete(`https://localhost:3248/University/DeleteUniversity/${name}`, { headers: headers });*/
-    return this.httpClient.delete(`https://madeforleaversms.azure-api.net/University/DeleteUniversity/${name}`, { headers: headers });
+    return this.httpClient.delete(`https://localhost:3248/University/DeleteUniversity/${name}`, { headers: headers });
   }
 
   /*getUniversity(email: string) sends get request to the C# back-end.*/
   getUniversity(email: string): Observable<University[]> {
     let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*");
     headers = headers.set("Authorization", email);
-    /*return this.httpClient.get<University[]>('https://localhost:3248/University/GetUniversity', { headers: headers });*/
-    return this.httpClient.get<University[]>('https://madeforleaversms.azure-api.net/University/GetUniversity', { headers: headers });
+    return this.httpClient.get<University[]>('https://localhost:3248/University/GetUniversity', { headers: headers });
   }
 
   /*postUniversity(university: University) sends post request to the C# back-end.*/
   postUniversity(university: University): Observable<any> {
     let headers = new HttpHeaders().set('Access-Control-Allow-Origin', "*").set('Content-Type', 'application/json');
-    /*return this.httpClient.post<University>('https://localhost:3248/University/PostUniversity', university, { headers: headers });*/
-    return this.httpClient.post<University>('https://madeforleaversms.azure-api.net/University/PostUniversity', university, { headers: headers });
+    return this.httpClient.post<University>('https://localhost:3248/University/PostUniversity', university, { headers: headers });
   }
 
 }
